f7c9d7bcc78d49ac50ccded8757e7c6d73309827
lindsayka9 <kat.lindsay.92@gmail.com>
2018-02-16 16:30:38 -0500

    Change config API Origin to deployed API
    https://powerful-cove-92841.herokuapp.com/

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,8 +2,8 @@
 
 const config = {
   apiOrigins: {
-    // production: 'https://ga-wdi-boston.herokuapp.com',
-    development: 'http://localhost:4741'
+    production: 'https://powerful-cove-92841.herokuapp.com/',
+    development: 'https://powerful-cove-92841.herokuapp.com/'
   }
 }
 
